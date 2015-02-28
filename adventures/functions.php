c42bb21fd9fd3ca54ce806df6a1b9a7f4b7c82d7
Kristian Andersen <hello@kristian.co>
2015-03-01 00:32:27 +0100

    Meta boxes

--- adventures/functions.php
+++ adventures/functions.php
@@ -1,5 +1,86 @@
 <?php
 
+// Add meta boxes for posts
+
+add_filter('rwmb_meta_boxes', 'rk_register_meta_boxes');
+
+function rk_register_meta_boxes($mb) {
+	$prefix = 'rk_';
+
+	$mb[] = array(
+		'id'		=> 'header_image',
+		'title'		=> 'Header Image',
+		'pages'		=> array('post'),
+		'context'	=> 'normal',
+		'priority'	=> 'high',
+
+		'fields'	=> array(
+			array(
+				'id'		=> $prefix . 'header_image',
+				'name'		=> 'Image',
+				'type'		=> 'image_advanced'
+			)
+		)
+	);
+
+	$mb[] = array(
+		'id'		=> 'tagline',
+		'title'		=> 'Tagline',
+		'pages'		=> array('post'),
+		'context'	=> 'normal',
+		'priority'	=> 'high',
+
+		'fields'	=> array(
+			array(
+				'id'		=> $prefix . 'tagline',
+				'name'		=> 'Tagline',
+				'type'		=> 'textarea'
+			)
+		)
+	);
+
+	$mb[] = array(
+		'id'		=> 'app-info',
+		'title'		=> 'App Info',
+		'pages'		=> array('post'),
+		'context'	=> 'normal',
+		'priority'	=> 'high',
+
+		'fields'	=> array(
+			array(
+				'id'		=> $prefix . 'name',
+				'name'		=> 'Name',
+				'type'		=> 'text'
+			),
+			array(
+				'id'		=> $prefix . 'appicon',
+				'name'		=> 'App Icon',
+				'type'		=> 'image_advanced'
+			),
+			array(
+				'id'		=> $prefix . 'appstore-ios',
+				'name'		=> 'App Store (iOS)',
+				'type'		=> 'text'
+			),
+			array(
+				'id'		=> $prefix . 'appstore-osx',
+				'name'		=> 'App Store (OS X)',
+				'type'		=> 'text'
+			),
+			array(
+				'id'		=> $prefix . 'website',
+				'name'		=> 'Website',
+				'type'		=> 'text'
+			),
+		)
+	);
+
+	return $mb;
+}
+
+// Suport Featured Images for posts
+add_theme_support( 'post-thumbnails' );
+
 function rk_retina_path($path) {
 	$comps = split("\.", $path);
 
@@ -61,5 +142,44 @@ function rk_meta_image() {
 		echo rk_theme_url("apple-touch-icon.png", false);
 	}
 }
+
+// Insert Images with captions using <figure> and <figcaption>
+
+if (!function_exists( 'atg_figure_caption' )) {
+	function atg_figure_caption( $output, $attr, $content ) {
+		/* We're not worried abut captions in feeds, so just return the output here. */
+		if ( is_feed() ) { return $output; }
+		/* Set up the default arguments. */
+		$defaults = array(
+			'id' => '',
+			'align' => 'alignnone',
+			'width' => '',
+			'caption' => ''
+		);
+		/* Merge the defaults with user input. */
+		$attr = shortcode_atts( $defaults, $attr );
+		/* If the width is less than 1, return the content wrapped between the [caption] tags. */
+		if ( 1 > $attr['width'] ) { return $content; }
+		/* Set up the attributes for the caption <figure>. */
+		$attributes = ( !empty( $attr['id'] ) ? ' id="' . esc_attr( $attr['id'] ) . '"' : '' );
+		$attributes .= ' class="wp-caption ' . esc_attr( $attr['align'] ) . '"';
+		//$attributes .= ' style="width: ' . esc_attr( $attr['width'] ) . 'px"';
+		/* Open the caption <figure>. */
+		$output = '<figure' . $attributes .'>';
+		/* Allow shortcodes for the content the caption was created for. */
+		$output .= do_shortcode( $content );
+		/* Append the caption text. */
+		if ($attr['caption'] !== '') {
+			$output .= '<figcaption class="wp-caption-text">' . $attr['caption'] . '</figcaption>';
+		}
+		/* Close the caption </figure>. */
+		$output .= '</figure>';
+		/* Return the formatted, clean figure & figcaption. */
+		return $output;
+	}
+	
+}
+
+add_filter( 'img_caption_shortcode', 'atg_figure_caption', 10, 3 );
 
 ?>
