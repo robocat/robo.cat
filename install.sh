ea3ceb4f5a7f232aa0b840dc403d1f6f8ac37885
Kristian Andersen <hello@kristian.co>
2015-02-28 23:34:14 +0100

    debug

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /var/repo/theme/
-make install && make clean && make build && make theme
+make install && make build && make theme
 rm -rf /var/www/wp-content/themes/robotheme
 cp -R ./theme/ /var/www/wp-content/themes/robotheme
