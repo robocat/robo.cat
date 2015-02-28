a0f4fe3f0324448f0046f7116b69a743e7849eed
Kristian Andersen <hello@kristian.co>
2015-02-28 23:07:49 +0100

    Theme development

--- theme/footer.php
+++
@@ -1,7 +0,0 @@
-	</div>
-	<script src="<?php rk_theme_url("js/vendor.js"); ?>" type="text/javascript" charset="utf-8"></script>
-	<script src="<?php rk_theme_url("js/app.js"); ?>" type="text/javascript" charset="utf-8" async defer></script>
-
-	<?php wp_footer(); ?>
-</body>
-</html>
