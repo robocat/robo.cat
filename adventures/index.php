0ba4ed58c3717b3e5290d819e65cfe1f17eed211
Kristian Andersen <hello@kristian.co>
2015-03-31 11:19:39 +0200

    Single page adventure mostly done

--- adventures/index.php
+++ adventures/index.php
@@ -13,12 +13,14 @@
                 <a href="//robo.cat/#team" id="nav-team">Team</a>
                 <a href="//adventures.robo.cat/" id="nav-adventures">Adventures</a>
                 <a href="#footer" id="nav-getintouch" class="scrollbased">Get in touch</a>
-                <a href="#share" id="share-tw" class="twitter">
-                    <?php rk_img("navigation-twitter.png"); ?>
-                </a>
-                <a href="#share" id="share-fb" class="facebook">
-                    <?php rk_img("navigation-facebook.png"); ?>
-                </a>
+                <div class="social">
+                    <a href="#share" id="share-tw" class="twitter">
+                        <?php rk_img("navigation-twitter.png"); ?>
+                    </a>
+                    <a href="#share" id="share-fb" class="facebook">
+                        <?php rk_img("navigation-facebook.png"); ?>
+                    </a>
+                </div>
             </div>
         </div>
     </nav>
